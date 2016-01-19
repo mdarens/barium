0d61b950df94549c27df103cf3266e2a932b640c
Mark D'Arensbourg <mdarens@gmail.com>
2016-01-19 09:44:09 -0600

    fixes #3 by leaving attr intact

--- src/example.js
+++ src/example.js
@@ -1,14 +1,33 @@
-import buryem from "./buryem";
+let buryem = require("buryem");
 
 let holder = {};
 
 const styles = buryem.create({
 	btn: {
-		border: "3px solid"
+		border: "3px solid #0af",
+        ':hover': {
+            borderColor: "#f20"
+        },
+        '@media (max-width: 800px)': {
+            display: "block",
+            width: "100%",
+            backgroundColor: "#0af",
+            color: "#fff",
+            ':before': {
+                content: "attr(title)",
+                color: "rgba(255,255,255,0.75)"
+            },
+            ':hover': {
+                backgroundColor: "#06f",
+                borderColor: "#02f"
+            }
+        }
 	}
-}, "hooray", holder);
+}, "my-hooray-button", holder);
 
-console.log({
-	styles: styles,
-	holder: holder
-});
+let htmlViewer = `
+<style>${holder.__cssText}</style>
+<button title="click me if you want" class="${styles.btn}">hooray</button>
+`;
+
+return htmlViewer;
